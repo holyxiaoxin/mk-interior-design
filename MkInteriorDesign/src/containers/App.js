c71152eb14de440fd46afdf8be6508a389ee8523
Jia Rong Lim <jr.loves.maths@gmail.com>
2016-03-20 14:44:22 +0800

    Use react-native-drawer-layout instead
    `react-native-drawer`library does not work with ios.
    It is only good for android.

--- MkInteriorDesign/src/containers/App.js
+++ MkInteriorDesign/src/containers/App.js
@@ -1,47 +1,67 @@
 'use strict';
 
-import Drawer from '../components/Drawer';
-import DrawerList from '../components/DrawerList';
-
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  TextInput
 } from 'react-native';
+import DrawerLayout from 'react-native-drawer-layout';
+import DrawerList from '../components/DrawerList';
 
 export default class App extends Component {
-  closeDrawer() {
-    this.refs.drawer.refs.drawer.close();
-  }
-
-  openDrawer() {
-    this.refs.drawer.refs.drawer.open();
+  constructor() {
+    super();
+    this.state = {};
   }
 
   render() {
-    const mainPage = (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          This is shared between ios and android
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+    const navigationView = (
+     <View style={[styles.container, {backgroundColor: '#fff'}]}>
+       <Text>Hello there!</Text>
+       <TouchableHighlight onPress={() => this.drawer.closeDrawer()}>
+         <Text>Close drawer</Text>
+       </TouchableHighlight>
+     </View>
+   );
+
+    // const mainPage = (
+    //   <View style={styles.container}>
+    //     <Text style={styles.welcome}>
+    //       Welcome to React Native!
+    //     </Text>
+    //     <Text style={styles.instructions}>
+    //       This is shared between ios and android
+    //     </Text>
+    //     <Text style={styles.instructions}>
+    //       Shake or press menu button for dev menu
+    //     </Text>
+    //   </View>
+    // );
+
+    // renderNavigationView={() => navigationView}
 
     return (
-      <Drawer
-        ref="drawer"
-        main={mainPage}
-        openDrawer={this.openDrawer}
-        closeDrawer={this.closeDrawer}
-      />
+      <DrawerLayout
+        onDrawerSlide={(e) => this.setState({drawerSlideOutput: JSON.stringify(e.nativeEvent)})}
+        onDrawerStateChanged={(e) => this.setState({drawerStateChangedOutput: JSON.stringify(e)})}
+        drawerWidth={300}
+        ref={(drawer) => { return this.drawer = drawer  }}
+        keyboardDismissMode="on-drag"
+        renderNavigationView={() => <DrawerList/>}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Content!</Text>
+          <Text>{this.state.drawerStateChangedOutput}</Text>
+          <Text>{this.state.drawerSlideOutput}</Text>
+          <TouchableHighlight onPress={() => this.drawer.openDrawer()}>
+            <Text>Open drawer</Text>
+          </TouchableHighlight>
+          <TextInput style={styles.inputField} />
+        </View>
+      </DrawerLayout>
     );
   }
 }

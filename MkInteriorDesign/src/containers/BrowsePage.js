885558dffcb4ff10ada07b09ac6bb0a2b683e7db
Jia Rong Lim <jr.loves.maths@gmail.com>
2016-03-26 13:21:54 +0800

    Add sample tinder cards for browse
    - Yet to polish, but it works!

--- MkInteriorDesign/src/containers/BrowsePage.js
+++ MkInteriorDesign/src/containers/BrowsePage.js
@@ -1,16 +1,64 @@
-import React, { Component, View, Text } from 'react-native';
+import React, { Component, StyleSheet, View, Text } from 'react-native';
 import NavBar from '../components/NavBar';
+import SwipeCards from 'react-native-swipe-cards';
+
+let Card = React.createClass({
+  render() {
+    return (
+      <View style={[styles.card, {backgroundColor: this.props.backgroundColor}]}>
+        <Text>{this.props.text}</Text>
+      </View>
+    )
+  }
+})
+
+const Cards = [
+  {text: 'Tomato', backgroundColor: 'red'},
+  {text: 'Aubergine', backgroundColor: 'purple'},
+  {text: 'Courgette', backgroundColor: 'green'},
+  {text: 'Blueberry', backgroundColor: 'blue'},
+  {text: 'Umm...', backgroundColor: 'cyan'},
+  {text: 'orange', backgroundColor: 'orange'},
+]
 
 export default class BrowsePage extends Component {
+  handleYup (card) {
+    console.log(`Yup for ${card.text}`)
+  }
+
+  handleNope (card) {
+    console.log(`Nope for ${card.text}`)
+  }
+
   render() {
+    const NoMoreCards = (
+      <Text>No more cards</Text>
+    )
+
     return(
       <View>
         <NavBar drawer={this.props.drawer} title="Discover Styles"/>
-        <Text style={{backgroundColor: '#000', color: 'blue', margin: 50}}>
-          Browse Page
-        </Text>
+          <SwipeCards
+            cards={Cards}
+
+            renderCard={(cardData) => <Card {...cardData} />}
+            renderNoMoreCards={() => NoMoreCards}
+
+            handleYup={this.handleYup}
+            handleNope={this.handleNope}
+          />
       </View>
 
     )
   }
 }
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
+    height: 300,
+  }
+})

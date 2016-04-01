993f48882fed93e92071c868d72464f8f381e94b
Ken Tan <kentan0130@gmail.com>
2016-04-01 21:45:38 +0800

    added base component with a  binding helper

---
+++ MkInteriorDesign/src/components/BaseComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react-native';
+
+class BaseComponent extends Component {
+    _bind(...methods) {
+        methods.forEach( (method) => this[method] = this[method].bind(this) );
+    }
+}
+
+export default BaseComponent;
+
+
+//this._bind('functionOne', 'functionTwo');

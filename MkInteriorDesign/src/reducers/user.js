15d50d39a1e06321aea353b094a46d04cfbe7a07
Jia Rong Lim <jr.loves.maths@gmail.com>
2016-04-10 23:32:09 +0800

    Add navbar hint animation on first time login
    - Stubbed this value in redux user store

--- MkInteriorDesign/src/reducers/user.js
+++ MkInteriorDesign/src/reducers/user.js
@@ -1,13 +1,19 @@
-import { LOGIN_WITH_FACEBOOK } from '../actions/user';
+import { LOGIN_WITH_FACEBOOK, SHOWN_NAVBAR_HINT } from '../actions/user';
 import Immutable from 'immutable';
 
-const initialState = Immutable.fromJS({});
+const initialState = Immutable.fromJS({
+  showNavbarHint: true // mock first time log in
+});
 
 export default function user(state = initialState, action = {}) {
   switch (action.type) {
     case LOGIN_WITH_FACEBOOK: {
       // TODO: Implement stubbed and remove log
       return state;
+    }
+    case SHOWN_NAVBAR_HINT: {
+      console.log('setting');
+      return state.set('showNavbarHint', false);
     }
     default:
       return state;
